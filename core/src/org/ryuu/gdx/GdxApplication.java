8d09826238ec1812ff092da25bca8fb064bc3b4e
DESKTOP-FQ830JM\Ryuu <2357622935@qq.com>
2022-12-20 22:46:30 +0800

    move SoundManger to Libgdx-Library

--- core/src/org/ryuu/gdx/GdxApplication.java
+++ core/src/org/ryuu/gdx/GdxApplication.java
@@ -1,6 +1,7 @@
 package org.ryuu.gdx;
 
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Group;
@@ -9,10 +10,8 @@ import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import lombok.Getter;
 import org.ryuu.gdx.scenes.scene2d.utils.ActionUtil;
-import org.ryuu.gdx.test.SoundManager;
-import org.ryuu.popup.PopUpAdapter;
+import org.ryuu.gdx.audio.SoundManager;
 import org.ryuu.popup.PopUpEvent;
-import org.ryuu.popup.PopUpEventArgs;
 
 import static com.badlogic.gdx.utils.Align.center;
 import static org.ryuu.gdx.$assets.texture2d.badlogic_jpg;
@@ -28,6 +27,7 @@ public class GdxApplication extends com.badlogic.gdx.Game {
     public void create() {
         setScreen(new StageScreen(1920, 1080));
         popUpTest();
+        soundMangerTest();
     }
 
     public StageScreen getStageScreen() {
@@ -38,21 +38,14 @@ public class GdxApplication extends com.badlogic.gdx.Game {
         return ((StageScreen) getScreen()).getStage();
     }
 
-    public void assetManagerTest() {
-        assetManager = new AssetManager();
-        assetManager.load($assets.sfx.hit_mp3, Sound.class);
-        assetManager.load($assets.sfx.button_mp3, Sound.class);
-        assetManager.load($assets.sfx.goal_mp3, Sound.class);
-        assetManager.finishLoading();
-    }
-
     public void soundMangerTest() {
+        assetManager = new AssetManager();
+        assetManager.load($assets.sfx.button_mp3, Sound.class);
+        assetManager.load($assets.sfx.win_mp3, Music.class);
+        assetManager.finishLoading();
         soundManager = new SoundManager(10, (path) -> assetManager.get(path, Sound.class));
         for (int i = 0; i < 10; i++) {
-            getStage().addAction(ActionUtil.delay(1, () -> {
-                SoundManager.SoundWrapper play = soundManager.play($assets.sfx.button_mp3);
-                getStage().addAction(ActionUtil.delay(1, () -> System.out.println(play)));
-            }));
+            getStage().addAction(ActionUtil.delay(i, () -> soundManager.play($assets.sfx.button_mp3, 1, 1, 1)));
         }
     }
 
@@ -62,8 +55,8 @@ public class GdxApplication extends com.badlogic.gdx.Game {
         Group group = getStageScreen().setSizeAsStage(new Group());
         getStage().addActor(group);
 
-        popUpEvent.add(1, new PopUpAdapter(), popUp -> popUp.getOnDispose().invoke());
-        popUpEvent.add(2, new PopUpAdapter(), popUp -> {
+        popUpEvent.add(1, popUp -> popUp.getOnDispose().invoke());
+        popUpEvent.add(2, popUp -> {
             Image logo = new Image(new Texture(badlogic_jpg));
             group.addActor(logo);
             align(logo, center);
@@ -73,7 +66,7 @@ public class GdxApplication extends com.badlogic.gdx.Game {
                     ActionUtil.of(() -> popUp.getOnDispose().invoke())
             ));
         });
-        popUpEvent.add(2, new PopUpAdapter(), popUp -> {
+        popUpEvent.add(2, popUp -> {
             Image logo = new Image(new Texture(badlogic_jpg));
             group.addActor(logo);
             align(logo, center);
@@ -83,7 +76,7 @@ public class GdxApplication extends com.badlogic.gdx.Game {
                     ActionUtil.of(() -> popUp.getOnDispose().invoke())
             ));
         });
-        popUpEvent.add(3, new PopUpAdapter(), popUp -> {
+        popUpEvent.add(3, popUp -> {
             Image logo = new Image(new Texture(badlogic_jpg));
             group.addActor(logo);
             align(logo, center);
@@ -92,7 +85,7 @@ public class GdxApplication extends com.badlogic.gdx.Game {
                     ActionUtil.of(() -> popUp.getOnDispose().invoke())
             ));
         });
-        popUpEvent.add(3, new PopUpAdapter(), popUp -> {
+        popUpEvent.add(3, popUp -> {
             Image logo = new Image(new Texture(badlogic_jpg));
             group.addActor(logo);
             align(logo, center);
